dd17e2648c70af9ab8e3cb0d666f2829d5c9dbda
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-20 19:24:52 -0500

    add_or_operaters

---
+++ add_or_operaters.py
@@ -0,0 +1,23 @@
+a = 20
+b = 10
+c = 0
+
+if a and b and c:
+    print("All the numbers have the value as True")
+else:
+    print("One number has a value as False")
+
+a = 20
+b = -14
+c = 0
+
+if a>0 or c>0:
+    print("Either of the numbers is greater than 0")
+else:
+    print("None of numbers are greater than 0")
+
+if b>0 or c>0:
+    print ("Either of the numbers is greater than 0")
+else:
+    print("None of numbers are greater than 0")
+

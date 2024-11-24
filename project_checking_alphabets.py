5acfc49fcd50941ff7e35e72fa821f8df476630d
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-23 20:46:11 -0500

    project_checking_alphabets

---
+++ project_checking_alphabets.py
@@ -0,0 +1,9 @@
+
+userinput =  input("Enter something to check if it is a alphabet or not:") 
+
+if userinput.isalpha():
+    print("The character entered is an alphabet")
+else:
+    print("The character entered is not an alphabet")
+
+
